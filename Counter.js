39f7bad30b7767cda834f9d806575e205e605331
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-06-18 09:51:08 +0530

    Create  Counter.js

---
+++ Counter.js
@@ -0,0 +1,28 @@
+function createCounter(start) {
+  let count = start;
+  return function() {
+    return count++;
+  }
+}
+
+/* 
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+ var createCounter = function(n) {
+    return function() {
+        return n++;
+    };
+};
+ 
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+ 
+
+ 
