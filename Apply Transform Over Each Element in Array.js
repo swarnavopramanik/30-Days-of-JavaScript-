6431faff39ac60e875133ba3f2166389139c427a
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-06-19 14:08:06 +0530

    Create Apply Transform Over Each Element in Array.js

---
+++ Apply Transform Over Each Element in Array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+/*  var map = function(arr, fn) {
+    return arr.map(fn);
+}; */
+ 
+ var map = function(arr, fn) {
+  var mappedArray = [];
+  for (var i = 0; i < arr.length; i++) {
+    mappedArray.push(fn(arr[i], i));
+  }
+  return mappedArray;
+} 
