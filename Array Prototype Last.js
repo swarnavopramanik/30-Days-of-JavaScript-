bae01861f35986961ba4a890c12b87b688ce00a4
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-06-19 22:17:25 +0530

    Create Array Prototype Last.js

---
+++ Array Prototype Last.js
@@ -0,0 +1,14 @@
+ Array.prototype.last = function() {
+    if (this.length == 0){
+        return -1;
+    }else{
+        return this[this.length - 1];
+    }
+};
+
+
+ 
+ 
+ /* Array.prototype.last = function() {
+  return this.length ? this[this.length-1] : -1;
+}; */
